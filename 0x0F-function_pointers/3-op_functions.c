fec8d941c21256e2fde71bc34e14d61ef3c85cf1
Imane-Bjr <61564697+Imane-Bjr@users.noreply.github.com>
2023-04-20 01:14:15 +0000

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,62 +1,62 @@
 #include "3-calc.h"
+
 /**
- * op_add - sum of a and b
- * @a: first int
- * @b: second int
- * Return: int
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - difference of a and b
- * @a: first int
- * @b: second int
- * Return: int
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - product of a and b
- * @a: first int
- * @b: second int
- * Return: int
+ * op_mul - calculates the product of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - result of the division of a by b
- * @a: first int
- * @b: second int
- * Return: int
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the division of a and b
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
+
 /**
- * op_mod - remainder of the division of a by b
- * @a: first int
- * @b: second int
- * Return: int
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided b
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
 
