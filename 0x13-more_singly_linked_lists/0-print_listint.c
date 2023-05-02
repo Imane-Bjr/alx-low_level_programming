1c33444b3d2e12f0150a5e716039d626ca03f5f8
Imane-Bjr <boujirimane1@gmail.com>
2023-05-02 10:52:02 -0700

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to struct listint_t
+ * Return: number of nods
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n_nodes = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		n_nodes++;
+	}
+
+	return (n_nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+ALX C: More singly linked lists readme file
