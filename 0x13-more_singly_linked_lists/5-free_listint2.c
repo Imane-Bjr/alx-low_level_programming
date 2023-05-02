ed924e5613aaeb37bcd186a4d9c3d89944ac8aa8
Imane-Bjr <boujirimane1@gmail.com>
2023-05-02 13:09:02 -0700

    free 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return;
 	current_nd = *head;
-	while (head)
+	while (*head)
 	{
 		*head = (*head)->next;
 		free(current_nd);
