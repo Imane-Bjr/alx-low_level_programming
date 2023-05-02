a350a9691d9cb7f51e9f1e00e8521bfd2a855cf5
Imane-Bjr <boujirimane1@gmail.com>
2023-05-02 14:44:58 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,6 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current_nd, *previous;
+	int result = -1;
 
 	for (current_nd = *head, previous = NULL;
 			current_nd != NULL && index--;
@@ -15,17 +16,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			current_nd = current_nd->next)
 		;
 	if (current_nd == NULL)
-		return (-1);
+		return (result);
 	if (previous == NULL)
 	{
 		*head = (*head)->next;
-		return (1);
+		result = 1;
 	}
 	else
 	{
 		previous->next = current_nd->next;
-		return (1);
+		result = 1;
 	}
 	free(current_nd);
+	return (result);
 }
 
