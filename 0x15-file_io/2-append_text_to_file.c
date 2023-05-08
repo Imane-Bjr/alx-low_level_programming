e8ab35eb3154b05808438a2346c921daa035751d
Imane-Bjr <61564697+Imane-Bjr@users.noreply.github.com>
2023-05-09 00:32:12 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,24 +7,20 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t numb_w;
-	int cont, leng = 0;
+	int numb_w, cont, leng = 0;
 
 	if (filename == NULL)
-		return (-1);
-
-	cont = open(filename, O_WRONLY | O_APPEND);
-	if (cont == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		for (leng = 0; text_content[leng];)
 			leng++;
-		numb_w = write(cont, text_content, leng - 1);
-		if (numb_w == -1)
-			return (-1);
 	}
+	cont = open(filename, O_WRONLY | O_APPEND);
+	numb_w = write(cont, text_content, leng);
+	if (count == -1 || numb_w == -1)
+			return (-1);
 	close(cont);
 	return (1);
 }
